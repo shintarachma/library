e0a3e46063ba58dd694972e13689759970f2d75a
satrioramadhan280 <62045532+satrioramadhan280@users.noreply.github.com>
2020-11-30 16:21:08 +0700

    Update BooksService.java

--- app/src/main/java/com/example/bookreview/utils/BooksService.java
+++ app/src/main/java/com/example/bookreview/utils/BooksService.java
@@ -1,12 +1,17 @@
 package com.example.bookreview.utils;
 
 import com.example.bookreview.model.BookResponse;
+import com.example.bookreview.model.Volume;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface BooksService {
     @GET("volumes")
     Call<BookResponse> getBooks(@Query("q") String query, @Query("maxResults") Integer maxResults);
+
+    @GET("volumes/{id}")
+    Call<Volume> getVolume(@Path("id") String volumeId);
 }
