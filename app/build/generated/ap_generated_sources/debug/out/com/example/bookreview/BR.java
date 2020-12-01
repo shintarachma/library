24b2853b71826c76996f023fd9d334564e4f7d94
shintarachma <shinta.rachmawati@binus.ac.id>
2020-12-01 12:47:29 +0700

    Adding navigation bar Re-arranging fragment_home, fragment_profile, fragment_search

--- app/build/generated/ap_generated_sources/debug/out/com/example/bookreview/BR.java
+++
@@ -1,9 +0,0 @@
-package com.example.bookreview;
-
-public class BR {
-  public static final int _all = 0;
-
-  public static final int loginViewModel = 1;
-
-  public static final int registerViewModel = 2;
-}
--- app/build/generated/source/dataBinding/trigger/debug/com/example/bookreview/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.example.bookreview;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
